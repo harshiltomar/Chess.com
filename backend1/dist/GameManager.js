11d7187d2940999184d5f030dcb7962996ebc861
Harshil Tomar <harshiltomar20@gmail.com>
2024-04-29 22:03:05 +0530

    1.1
    class initialised, basic structure

---
+++ backend1/dist/GameManager.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GameManager = void 0;
+class GameManager {
+    constructor() {
+        this.games = [];
+    }
+}
+exports.GameManager = GameManager;
